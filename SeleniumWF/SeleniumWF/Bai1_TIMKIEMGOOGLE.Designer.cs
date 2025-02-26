1f515b197ed77bc920f73f3ed3a3c895c74ee9c0
Theanh130124 <theanhtran13012004@gmail.com>
2025-02-26 13:15:59 +0700

    Udpadte Selenium

---
+++ SeleniumWF/SeleniumWF/Bai1_TIMKIEMGOOGLE.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace SeleniumWF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(399, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(279, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Chuyển hướng đên gg";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(79, 237);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(299, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Chuyển hướng đến gg _ Cách 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(285, 126);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(265, 22);
+            this.txtUrl.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(148, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Url";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(243, 448);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(316, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Tìm kiếm trên gg luôn ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(271, 371);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(264, 22);
+            this.txtSearch.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 374);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Nội dung tìm kiếm trên google";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 557);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SeleniumWF/SeleniumWF/Bai1_TIMKIEMGOOGLE.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumWF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebDriver driver = new ChromeDriver(); // nạp driver
+            driver.Navigate().GoToUrl("https://www.google.com/"); // Chuyển hướng đến gg
+
+            //driver.Navigate().Back() -> lùi lại || . Foward() -> tiến lên || .Refesh() -> tải lại trang
+            
+            driver.Close(); // Mở xong rồi tắt liền chỉ tắt 1 tab
+
+
+            //driver.Quit(); // đóng tất cả các tab 
+
+
+            //String title = driver.Title;  -> Tiêu đề web
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Cách 2 
+            WebDriver driver_2 = new ChromeDriver();
+            //driver_2.Url = "https://www.google.com/";
+            // hoặc chổ này có thể gán txt trên WF để người dùng có thể điền vào
+            driver_2.Url = txtUrl.Text;
+            driver_2.Navigate();
+
+            Thread.Sleep(1000); // ngủ 1 s
+
+            //String src = driver_2.PageSource;  -> lấy hết src code
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            ChromeDriverService chrome = ChromeDriverService.CreateDefaultService();
+            chrome.HideCommandPromptWindow = true;
+            //2 dòng này để ẩn màn hình cmd đen đi nhớ bỏ chrome vào trong new
+
+
+                WebDriver driver_3 = new ChromeDriver(chrome);
+            driver_3.Navigate().GoToUrl("https://www.google.com/");
+
+            // bắt đầu lấy element -> Mình sẽ lấy title và url sau khi đã tìm kím in ra Console
+            IWebElement element = driver_3.FindElement(By.Name("q"));
+            element.SendKeys(txtSearch.Text);
+            element.Submit();
+            Thread.Sleep(8000);
+            //element.Clear(); -> xóa element
+
+
+            //
+             ReadOnlyCollection<IWebElement> elments = driver_3.FindElements(By.CssSelector(".g h3"));
+            foreach ( var element_1 in  elments )
+            {
+                Console.WriteLine(element_1.Text);
+                Console.WriteLine("====================================");
+            }    
+
+            driver_3.Close();
+
+            
+
+        }
+    }
+}
